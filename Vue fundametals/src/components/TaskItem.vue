a395893e765f9c7e41bd844430ba9eb0bb377f98
Melichris <melichristian51@gmail.com>
2026-08-30 02:21:04 +0100

    add TaskItem component with props for title, priority, and completion status

--- Vue fundametals/src/components/TaskItem.vue
+++ Vue fundametals/src/components/TaskItem.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
     <p>{{ }}</p>
+    <input type="checkbox" name="" id="" @click="toggleCompleted">
   </div>
 </template>
 <script lang="ts">
+import Task from "@/components/TaskTracker.vue"
+import { computed } from "vue"
+const toggleCompleted: <Task> = computed(() => {
+  Task.
+})
+
 defineProps<{
   title: string,
   priority: number,
   completed: boolean
 }>()
+
 </script>
---
+++ Vue fundametals/src/components/TaskList.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TaskItem from './TaskItem.vue';
+</script>
+
+<template>
+  <div>
+    <p>
+      <TaskItem title="Buy milk" :priority="2" :completed="false" />
+    </p>
+  </div>
+</template>
